fa4b883ee2a9d4c40617ca8a0b4c6a9f85cf5b43
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-05-08 22:57:31 -0700

    changed server url for deployment

--- src/axiosCalls/axiosCalls.js
+++ src/axiosCalls/axiosCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const accessToken = JSON.parse(localStorage.getItem("user"))?.accessToken || "";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://capstone-serveer.herokuapp.com",
   headers: {
     token: `Bearer ${accessToken}`,
   },
