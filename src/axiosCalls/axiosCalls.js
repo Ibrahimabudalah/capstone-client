ef19d4016fed75fa5218ad69ae6e99d9eb7b0a8a
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2022-12-15 23:59:46 -0800

    updated server URL

--- src/axiosCalls/axiosCalls.js
+++ src/axiosCalls/axiosCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const accessToken = JSON.parse(localStorage.getItem("user"))?.accessToken || "";
 
 const instance = axios.create({
-  baseURL: "https://your-fitness--pal.herokuapp.com",
+  baseURL: "https://ubiquitous-bavarois-14c6b2.netlify.app/",
   headers: {
     token: `Bearer ${accessToken}`,
   },
